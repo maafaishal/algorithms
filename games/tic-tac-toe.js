cefea64133e7edef6b3ad5717b3ae59377a03319
Faishal <maafaishal@gmail.com>
2024-08-08 09:07:18 +0700

    feat: tic tac toe

---
+++ games/tic-tac-toe.js
@@ -0,0 +1,62 @@
+const allEqual = (arr) => arr.every((val) => val !== "" && val === arr[0]);
+
+const checkWinner = (board) => {
+  const size = board.length;
+
+  // Check rows
+  for (let row = 0; row < size; row++) {
+    if (allEqual(board[row])) {
+      return board[row][0];
+    }
+  }
+
+  // Check columns
+  for (let col = 0; col < size; col++) {
+    const column = [];
+
+    for (let row = 0; row < size; row++) {
+      column.push(board[row][col]);
+    }
+
+    if (allEqual(column)) {
+      return column[0];
+    }
+  }
+
+  // Check diagonal
+  let mainDiagonal = [];
+  let antiDiagonal = [];
+
+  for (let i = 0; i < size; i++) {
+    mainDiagonal.push(board[i][i]);
+    antiDiagonal.push(board[i][size - 1 - i]);
+  }
+
+  if (allEqual(mainDiagonal)) {
+    return mainDiagonal[0];
+  }
+
+  if (allEqual(antiDiagonal)) {
+    return antiDiagonal[0];
+  }
+};
+
+const BOARD_3X3 = [
+  ["O", "O", "X"],
+  ["O", "X", "O"],
+  ["O", "X", "X"],
+];
+
+const BOARD_5X5 = [
+  ["O", "O", "X", "O", "X"],
+  ["O", "X", "O", "X", "O"],
+  ["O", "X", "X", "O", "O"],
+  ["O", "X", "X", "O", "O"],
+  ["O", "X", "X", "O", "O"],
+];
+
+const BOARD = BOARD_5X5;
+
+const winner = checkWinner(BOARD);
+
+console.log("winner", winner);
